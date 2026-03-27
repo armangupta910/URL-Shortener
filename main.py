b33a0f140df32cf4e738b41f3cd31c73b71c6ba8
armangupta910 <guptaarman910@gmail.com>
2026-03-28 00:28:46 +0530

    CORS issue fixed

--- main.py
+++ main.py
@@ -13,9 +13,17 @@ import uuid
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from slowapi.util import get_remote_address
+from fastapi.middleware.cors import CORSMiddleware
 
 limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 🔥 allow ALL origins
+    allow_credentials=True,
+    allow_methods=["*"],  # allow all HTTP methods
+    allow_headers=["*"],  # allow all headers
+)
 app.state.limiter = limiter
 snowflake_gen = SnowflakeGenerator(1)
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
